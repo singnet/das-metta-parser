78d1e11b0e2f2f9bf907a2094b14a9acc00b6433
Andre Senna <andre.senna@gmail.com>
2024-03-19 08:52:54 -0300

    Minor fix

--- tests/unit/test_syntax_check.sh
+++ tests/unit/test_syntax_check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#cat tests/data/edges.metta | bin/syntax_check &&\
-cat tests/data/type_defs.metta | bin/syntax_check &&\
-cat tests/data/gencode_nodes.metta | bin/syntax_check &&\
-cat tests/data/gencode_edges.metta | bin/syntax_check
+echo "tests/data/special_cases.metta" &&\
+./bin/syntax_check tests/data/special_cases.metta &&\
+echo "tests/data/animals.metta" &&\
+./bin/syntax_check tests/data/animals.metta &&\
+echo "tests/data/type_defs.metta" &&\
+./bin/syntax_check tests/data/type_defs.metta &&\
+echo "tests/data/gencode_edges.metta" &&\
+./bin/syntax_check tests/data/gencode_edges.metta
