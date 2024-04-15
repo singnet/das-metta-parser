c9ed8612fc1c4d4cb6ec65f3527f60e2ca4e576f
Andre Senna <andre.senna@gmail.com>
2024-04-15 13:08:25 -0300

    Added test cases

--- tests/unit/test_syntax_check.sh
+++ tests/unit/test_syntax_check.sh
@@ -6,7 +6,11 @@ echo "tests/data/animals.metta" &&\
 ./scripts/run.sh syntax_check ./tests/data/animals.metta &&\
 echo "tests/data/type_defs.metta" &&\
 ./scripts/run.sh syntax_check ./tests/data/type_defs.metta &&\
+echo "tests/data/gencode_nodes.metta" &&\
+./scripts/run.sh syntax_check ./tests/data/gencode_nodes.metta &&\
 echo "tests/data/gencode_edges.metta" &&\
 ./scripts/run.sh syntax_check ./tests/data/gencode_edges.metta &&\
+echo "tests/data/hocomoco_nodes.metta" &&\
+./scripts/run.sh syntax_check ./tests/data/hocomoco_nodes.metta &&\
 echo "" &&\
 echo "ALL UNIT TESTS OK"
